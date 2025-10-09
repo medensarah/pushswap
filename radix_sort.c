7aefc5b9b84f5d727407f02ea83479dc711a20d2
Sarah Medeneche <meden.sarah@gmail.com>
2025-10-09 18:27:58 +0200

    color makefile ?

--- radix_sort.c
+++ radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 15:53:26 by smedenec          #+#    #+#             */
-/*   Updated: 2025/10/09 18:01:45 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/10/09 18:26:51 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	radix(t_list **stack_a, t_list **stack_b, int len_a)
 
 	max_bits = 0;
 	while ((len_a >> max_bits) != 0)
-	max_bits++;
+		max_bits++;
 	i = 0;
 	while (i < max_bits)
 	{
